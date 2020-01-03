0b252ac1011d8b704e76358711e4eabd4bfc31c3
Ashraful Islam <ashraful@Ashrafuls-MacBook-Pro.local>
2020-01-03 15:04:52 +0600

    Added example vue component to run example

---
+++ src/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Basic Example</h2>
+    <awesome-select/>
+  </div>
+</template>
+
+<script>
+import AwesomeSelect from './AwesomeSelect'
+
+export default {
+  name: 'Example',
+  components: { AwesomeSelect }
+}
+</script>
